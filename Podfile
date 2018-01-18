f494c4a1ead6fc0be3aaca0af9d9952e4e9b45cd
Ivan Almada <ivan.almada@green-light.com>
2018-01-18 11:05:58 +0100

    Initial commit.

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '9.0'
+use_frameworks!
+
+target 'PokeSpin' do
+  pod 'RealmSwift', '~> 3.1.0'
+end
---
+++ PokeSpin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokeSpin
+//
+//  Created by Ivan Almada on 18/01/2018.
+//  Copyright © 2018 Ivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - UIViewController
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
