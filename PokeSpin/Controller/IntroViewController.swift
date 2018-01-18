fa3906926497cda79cffdc8aea89ad6281064cc4
Ivan Almada <ivan.almada@green-light.com>
2018-01-18 12:49:56 +0100

    Added Intro View Controller without reset progress implementation.

--- PokeSpin/Controller/IntroViewController.swift
+++ PokeSpin/Controller/IntroViewController.swift
@@ -10,10 +10,42 @@ import UIKit
 
 class IntroViewController: BaseViewController {
 
+    @IBOutlet var infoTextView: UITextView!
+
     // MARK: - UIViewController
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    // MARK: - Setup UI
+
+    func setupUI() {
+        infoTextView.backgroundColor = UIColor.creamyBlue
+
+        view.backgroundColor = UIColor.creamyBlue
+    }
+
+    // MARK: - IBActions
+
+    @IBAction func triggerSegueTapped(sender: AnyObject) {
+        performSegue(withIdentifier: Constants.SegueIdentifier.openPokeDex.rawValue, sender: self)
+    }
+
+    @IBAction func resetProgressTapped(sender: AnyObject) {
+        let alertController = UIAlertController(title: "Confirm reset progress.", message: "Are you sure you want to erase your precious Pokemon?", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alertController.addAction(UIAlertAction(title: "Reset progress", style: .destructive, handler: { [weak self] (action) in
+            self?.resetProgress()
+        }))
+
+        present(alertController, animated: true, completion: nil)
+    }
+
+    // MARK: - Helpers
+
+    func resetProgress() {
+
     }
 
 }
---
+++ PokeSpin/Extensions/AppColors.swift
@@ -0,0 +1,13 @@
+//
+//  AppColors.swift
+//  PokeSpin
+//
+//  Created by Ivan Almada on 18/01/2018.
+//  Copyright © 2018 Ivan. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let creamyBlue = UIColor(red: 222.0/255.0, green: 241.0/255.0, blue: 252.0/255.0, alpha: 1.0)
+}
