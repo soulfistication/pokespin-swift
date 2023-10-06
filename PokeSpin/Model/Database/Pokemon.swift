594023b33df1548d62f9a58dba114215a848ded4
Ivan Almada <ivan.almada@me.com>
2023-10-06 13:25:22 +0200

    Fix indentation

--- PokeSpin/Model/Database/Pokemon.swift
+++ PokeSpin/Model/Database/Pokemon.swift
@@ -13,7 +13,7 @@ class Pokemon: NSManagedObject, Decodable {
     required convenience init(from decoder: Decoder) throws {
         
         guard let context = decoder.userInfo[CodingUserInfoKey.managedObjectContext] as? NSManagedObjectContext else {
-          throw DecoderConfigurationError.missingManagedObjectContext
+            throw DecoderConfigurationError.missingManagedObjectContext
         }
 
         self.init(context: context)
@@ -27,19 +27,19 @@ class Pokemon: NSManagedObject, Decodable {
     }
     
     enum CodingKeys: String, CodingKey {
-            case id = "id"
-            case name = "name"
-            case weight = "weight"
-            case height = "height"
-            case baseExperience = "base_experience"
+        case id = "id"
+        case name = "name"
+        case weight = "weight"
+        case height = "height"
+        case baseExperience = "base_experience"
     }
     
 }
 
 extension CodingUserInfoKey {
-  static let managedObjectContext = CodingUserInfoKey(rawValue: "managedObjectContext")!
+    static let managedObjectContext = CodingUserInfoKey(rawValue: "managedObjectContext")!
 }
 
 enum DecoderConfigurationError: Error {
-  case missingManagedObjectContext
+    case missingManagedObjectContext
 }
