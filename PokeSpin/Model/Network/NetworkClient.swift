b4f932705623968f3bb132627dae6e052194b7a6
Ivan Almada <ivan.almada@me.com>
2023-10-11 10:19:55 +0200

    Remove unused network client func

--- PokeSpin/Model/Network/NetworkClient.swift
+++ PokeSpin/Model/Network/NetworkClient.swift
@@ -34,74 +34,27 @@ struct NetworkClient {
     }
     
     func requestJSONData(pokemon: Int, completion: @escaping (Result<Data, Error>) -> Void) {
-        
         guard let url = URL(string: "\(baseURL)/\(apiVersion)/\(endpoint)/\(pokemon)") else { return }
-        
         let urlSession = URLSession(configuration: URLSessionConfiguration.default)
         let dataTask = urlSession.dataTask(with: url) { data, response, error in
-            
             guard error == nil else {
                 completion(.failure(error!))
                 return
             }
-            
             guard let response = response as? HTTPURLResponse else {
                 completion(.failure(APIError.badResponse))
                 return
             }
-            
             guard response.statusCode == 200 else {
                 completion(.failure(APIError.badStatusCode))
                 return
             }
-            
             guard let data = data else {
                 completion(.failure(APIError.emptyData))
                 return
             }
-            
             completion(.success(data))
         }
-        
-        dataTask.resume()
-    }
-
-    func requestJSONString(pokemon: Int, completion: @escaping (Result<String, Error>) -> Void) {
-        
-        guard let url = URL(string: "\(baseURL)/\(apiVersion)/\(endpoint)/\(pokemon)") else { return }
-
-        let urlSession = URLSession(configuration: URLSessionConfiguration.default)
-        
-        let dataTask = urlSession.dataTask(with: url) { data, response, error in
-            
-            guard error == nil else {
-                completion(.failure(error!))
-                return
-            }
-            
-            guard let response = response as? HTTPURLResponse else {
-                completion(.failure(APIError.badResponse))
-                return
-            }
-            
-            guard response.statusCode == 200 else {
-                completion(.failure(APIError.badStatusCode))
-                return
-            }
-
-            guard let data = data else {
-                completion(.failure(APIError.emptyData))
-                return
-            }
-            
-            guard let result = String(data: data, encoding: .utf8) else {
-                completion(.failure(APIError.wrongEncoding))
-                return
-            }
-
-            completion(.success(result))
-        }
-        
         dataTask.resume()
     }
 }
